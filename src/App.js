7ecba8cf839bacbad7eace1c3e476c19a17da220
VaggelisKa <e.karavasileiadis@gmail.com>
2020-10-18 16:42:43 +0300

    Setting user object observable when user state changes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,37 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
 import SigninAndSignUpPage from './pages/signin-and-signup-page/signin-and-signup-page.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SigninAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SigninAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
