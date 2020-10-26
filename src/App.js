aca0bfbed8dcbbfdd480f4013379dc0f85786977
VaggelisKa <e.karavasileiadis@gmail.com>
2020-10-26 16:42:47 +0200

    Start creation of checkout page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { setCurrentUser } from './redux/user/user.actions';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import SigninAndSignUpPage from './pages/signin-and-signup-page/signin-and-signup-page.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions'
+
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -46,6 +49,7 @@ class App extends React.Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SigninAndSignUpPage />)} />
+          <Route exact path="/checkout" component={CheckoutPage} />
         </Switch>
       </div>
     );
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
@@ -8,14 +9,16 @@ import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
     <div className='cart-dropdown'>
         <div className="cart-items">
             {
-                cartItems.map(item => <CartItem key={item.id} item={item}/>)
+                cartItems.length > 0 ?
+                cartItems.map(item => <CartItem key={item.id} item={item}/>) 
+                : <span className="empty-message">Your cart is empty</span>
             }
         </div>
-        <Button>GO TO CHECKOUT</Button>
+        <Button onClick={() => history.push('/checkout')}>GO TO CHECKOUT</Button>
     </div>
 );
 
@@ -23,4 +26,4 @@ const mapStateToProps = (state) => ({
     cartItems: selectCartItems(state)
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
