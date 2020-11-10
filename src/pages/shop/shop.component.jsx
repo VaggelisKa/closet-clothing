135fa6edb26e20ea3a968725a6967b276310ef09
VaggelisKa <e.karavasileiadis@gmail.com>
2020-11-10 19:55:41 +0200

    Adding collection-overview container

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,12 +6,11 @@ import { createStructuredSelector } from 'reselect';
 import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPage from '../collection/collection.component';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends Component {
@@ -26,7 +25,7 @@ class ShopPage extends Component {
 
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} render={props => <CollectionsOverviewWithSpinner isLoading={!isCollectionsLoaded} {...props}/>} />
+                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
                 <Route path={`${match.path}/:collectionId`} render={props => <CollectionsPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />}/>
             </div>
         );
