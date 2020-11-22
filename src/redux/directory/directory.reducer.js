45851e3a356f6c0700cfd75ed4b708003172c75e
VaggelisKa <e.karavasileiadis@gmail.com>
2020-11-22 15:42:49 +0200

    Changing background images in collection preview

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -8,26 +8,26 @@ const INITIAL_STATE = {
           },
           {
             title: 'jackets',
-            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            imageUrl: 'https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
             id: 2,
             linkUrl: 'shop/jackets'
           },
           {
             title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            imageUrl: 'https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
             id: 3,
             linkUrl: 'shop/sneakers'
           },
           {
             title: 'womens',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+            imageUrl: 'https://images.unsplash.com/photo-1593380090147-a2192b72a9ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1225&q=80',
             size: 'large',
             id: 4,
             linkUrl: 'shop/womens'
           },
           {
             title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/closet-clothing.appspot.com/o/Collection-preview%20photos%2FXangel.jpg?alt=media&token=8198c427-06df-489a-96a3-b0d36d185bf0',
             size: 'large',
             id: 5,
             linkUrl: 'shop/mens'
