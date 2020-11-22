dfa5bd949f7e063a82bb9393b798845dccea6f15
VaggelisKa <e.karavasileiadis@gmail.com>
2020-11-22 16:47:10 +0200

    Using userHistory and routeMatch hooks on menu items to navigate

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import {withRouter} from 'react-router-dom';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-export const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
-    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-        <div         
-            style={{backgroundImage: `url(${imageUrl})`}} 
-            className= "background-image"
-        />
-        <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-)
+export const MenuItem = ({title, imageUrl, size, linkUrl}) => {
+    let history = useHistory();
+    let match = useRouteMatch();
 
-export default withRouter(MenuItem);
+    const handleClick = () => {
+        history.push(`${match.url}${linkUrl}`)
+    }
+
+    return (
+        <div className={`${size} menu-item`} onClick={handleClick}>
+            <div         
+                style={{backgroundImage: `url(${imageUrl})`}} 
+                className= "background-image"
+            />
+            <div className="content">
+                <h1 className="title">{title}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    )
+}
+
+export default MenuItem;
