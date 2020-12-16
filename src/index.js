5770221e5f056be78a909f7091e2d4a047f078ac
VaggelisKa <e.karavasileiadis@gmail.com>
2020-12-16 14:13:01 +0200

    Update code for PWA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
+
 import App from './App';
 
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -21,4 +23,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
